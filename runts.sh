75738b355b6b8b51a6f560217628bb848a6a5b0a
keyxmakerx <keyxmakerx@gmail.com>
2015-02-13 10:39:20 -0600

    Update runts.sh

--- runts.sh
+++ runts.sh
@@ -3,6 +3,7 @@
 [[ $(id --user) -ne 0 ]] && { echo "Sudoing..." ; sudo $(readlink -e $0) "$@" ; exit 0 ; } # sudo ourself
 LOGFILE=/usr/scriptlog.txt
 mkdir /usr/share/Teamspeak
+mkdir /usr/share/Teamspeak/Server
 cd /usr/share/Teamspeak
 wget -O ts-package.tar.gz http://dl.4players.de/ts/releases/3.0.11.2/teamspeak3-server_linux-amd64-3.0.11.2.tar.gz &&\
 tar xvfz ts-package.tar.gz -C Server --strip-components 1
